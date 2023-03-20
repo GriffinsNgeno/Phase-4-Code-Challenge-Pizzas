892c7be67885bf2c76aa0ae589b520c35714234d
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-20 23:12:15 +0300

    Updated create request

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantPizzasController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     # GET /restaurant_pizzas/
     def index
@@ -7,6 +8,18 @@ class RestaurantPizzasController < ApplicationController
 
     # POST /restaurant_pizzas/
     def create
-
+        restaurant_pizza = RestaurantPizza.create(restaurant_pizza_params)
+        render json: restaurant_pizza
     end
+
+    private
+
+    def restaurant_pizza_params
+        params.permit(:price, :pizza_id, :restaurant_id)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+      end
+    
 end
