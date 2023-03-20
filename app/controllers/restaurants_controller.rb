819a3f4455eccc9e9397df4de869a31433c711b4
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-20 21:54:51 +0300

    Updated the routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,12 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+    # GET /restaurants/
     def index
         render json: Restaurant.all
     end
 
+    # GET /restaurants/:id
     def show
         restaurants = restaurant_params
         render json: restaurants
@@ -20,4 +23,10 @@ class RestaurantsController < ApplicationController
     def restaurant_params
         Restaurant.find_by(id: params[:id])
     end
+
+    def render_not_found_response
+        render json: { error: "Restaurant not found" }, status: :not_found
+      end
+    
+    
 end
