cce13b562896ed9e87a89bc0bc8f114783e39835
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-20 22:57:02 +0300

    Added comments

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,9 +1,12 @@
 class RestaurantPizzasController < ApplicationController
 
+    # GET /restaurant_pizzas/
     def index
         render json: RestaurantPizza.all
     end
+
+    # POST /restaurant_pizzas/
     def create
-        
+
     end
 end
