5b6e07c3eb41b88d4febf5f162e14e1c5352649c
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-20 18:11:18 +0300

    Created restaurant routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,23 @@
 class RestaurantsController < ApplicationController
+
+    def index
+        render json: Restaurant.all
+    end
+
+    def show
+        restaurants = restaurant_params
+        render json: restaurants
+    end
+
+    def destroy
+        restaurant = restaurant_params
+        restaurant.destroy
+        head :no_content
+    end
+
+    private
+
+    def restaurant_params
+        Restaurant.find_by(id: params[:id])
+    end
 end
