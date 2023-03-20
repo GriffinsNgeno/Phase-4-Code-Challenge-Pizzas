7b33d087b2d6ec026e068ef50133784f2e3e9a83
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-03-20 17:39:49 +0300

    Added validation

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,5 +1,8 @@
 class RestaurantPizza < ApplicationRecord
     belongs_to :pizza
     belongs_to :restaurant
-    
+
+    # `price` between 1 and 30
+    validates :price, numericality: { greater_than: 0, less_than: 31 }
+
 end
